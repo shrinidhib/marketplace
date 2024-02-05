8cacf85739b960c65712b6c3b48579a2a4d07f84
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-02-05 18:40:16 +0530

    Marketplace

---
+++ marketplace/item/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from .models import Item
+
+input_classes="w-full py-4 px-6 rounded-xl border"
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model=Item
+        fields=['category','name','description','price','image']
+
+        widgets={
+            'category': forms.Select(attrs={
+                'class': input_classes
+            }),
+            'name': forms.TextInput(attrs={
+                'class': input_classes
+            }),
+            'description': forms.TextInput(attrs={
+                'class': input_classes
+            }),
+            'Price': forms.TextInput(attrs={
+                'class': input_classes
+            }),
+            'image': forms.FileInput(attrs={
+                'class': input_classes
+            }),
+        }
+
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model=Item
+        fields=['name','description','price','image','is_sold']
+
+        widgets={
+            'name': forms.TextInput(attrs={
+                'class': input_classes
+            }),
+            'description': forms.TextInput(attrs={
+                'class': input_classes
+            }),
+            'Price': forms.TextInput(attrs={
+                'class': input_classes
+            }),
+            'image': forms.FileInput(attrs={
+                'class': input_classes
+            }),
+        }
